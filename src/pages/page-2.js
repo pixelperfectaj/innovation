7b229678db103e78662fa85c532913dcc0b0c19c
anjenkin <121010+anjenkin@users.noreply.github.com>
2021-07-23 11:43:56 -0400

    initial commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,6 +3,42 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Octokit } from "@octokit/rest";
+import { Base64 } from 'js-base64';
+
+const key = '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';
+const octokit = new Octokit({
+  auth: key,
+});
+
+async function getSHA(path) {
+  const result = await octokit.repos.getContent({
+    owner: "pixelperfectaj",
+    repo: "innovation",
+    path,
+  });
+
+  const sha = result?.data?.sha;
+
+  return sha;
+}
+
+async function commitArticle() {
+  console.log('hi')
+  const path = `test.mdx`;
+  const sha = await getSHA(path);
+//
+//   // const result = await octokit.repos.createOrUpdateFileContents({
+//   //   owner: "pixelperfectaj",
+//   //   repo: "innovation",
+//   //   path,
+//   //   message: `Add article 1`,
+//   //   content: Base64.encode(`some content here`),
+//   //   sha,
+//   // });
+//   //
+//   // return result?.status || 500;
+}
 
 const SecondPage = () => (
   <Layout>
@@ -10,6 +46,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <button onClick={commitArticle}>Submit!</button>
   </Layout>
 )
 
